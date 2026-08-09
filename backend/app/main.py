12f25df08c842b13269470c1811a036c55384dc8
michellelee6 <michelleklee07@gmail.com>
2026-08-09 16:18:22 -0700

    added vercel url to backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -113,7 +113,8 @@ class ChatResponse(BaseModel):
 app = FastAPI(title="SyllabusSync API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    # allow_origins=["http://localhost:5173"],
+    allow_origins=["syllabus-sync-omega.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
